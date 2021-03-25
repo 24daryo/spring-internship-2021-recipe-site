f9fad668fda4c378c8826005bc13031acd5cb49d
Nishida Ryousuke <55966582+24daryo@users.noreply.github.com>
2021-03-25 15:36:49 +0900

    type_error_fixed

--- lib/recipe.ts
+++ lib/recipe.ts
@@ -7,11 +7,16 @@ export type RecipeType = {
   title: string;
   description: string;
   image_url: string | null;
+  author: {
+    user_name: string;
+  };
+  published_at: string;
   steps: string[];
   ingredients: {
     name: string;
     quantity: string;
   }[];
+  related_recipes: number[];
 };
 
 export type RecipeListType = {
